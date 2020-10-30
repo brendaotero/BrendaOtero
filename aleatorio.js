6849dedc929bca63ec8f8e7c0ef0ae53b4aa68b2
brendaotero <brendaotero2001@gmail.com>
2020-10-30 00:34:36 -0300

    correciones

--- aleatorio.js
+++
@@ -1,7 +0,0 @@
-function aleatorios (min, max){
-
- for (let i = 1; i <= 10; ++i) {
- 	int aleatorio = (int) (Math.random() * (max - min) + min );
- 	system.out.println(aleatorio);
- 	 }
-  }	 
--- consola.js
+++
@@ -1,5 +0,0 @@
-function consola ()
-{
-
-	console.log("holamundo");
-}
