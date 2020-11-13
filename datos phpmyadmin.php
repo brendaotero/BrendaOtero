d8baba9a9ac9df4ecd48f9be5d278046a35cf0bb
brendaotero <brendaotero2001@gmail.com>
2020-11-13 11:00:46 -0300

    añadido

---
+++ datos phpmyadmin.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.2
+ */
+
+/**
+ * Database `trabajo`
+ */
+
+/* `trabajo`.`paises` */
+$paises = array(
+  array('id' => '1','nombres' => 'argentina'),
+  array('id' => '2','nombres' => 'uruguay')
+);
+
+/* `trabajo`.`provincia` */
+$provincia = array(
+  array('id' => '1','nombre' => 'buenos aires'),
+  array('id' => '2','nombre' => 'canelones'),
+  array('id' => '1','nombre' => 'corrientes'),
+  array('id' => '1','nombre' => 'catamarca'),
+  array('id' => '1','nombre' => 'salta'),
+  array('id' => '1','nombre' => 'jujuy'),
+  array('id' => '1','nombre' => 'cordoba'),
+  array('id' => '1','nombre' => 'formosa'),
+  array('id' => '1','nombre' => 'santa cruz'),
+  array('id' => '1','nombre' => 'chubut'),
+  array('id' => '1','nombre' => 'tierra del fuego'),
+  array('id' => '1','nombre' => 'la pampa'),
+  array('id' => '1','nombre' => 'san juan'),
+  array('id' => '1','nombre' => 'san luis'),
+  array('id' => '1','nombre' => 'santa fe'),
+  array('id' => '1','nombre' => 'misiones'),
+  array('id' => '1','nombre' => 'mendoza'),
+  array('id' => '1','nombre' => 'la rioja'),
+  array('id' => '1','nombre' => 'entre rios'),
+  array('id' => '1','nombre' => 'rio negro'),
+  array('id' => '1','nombre' => 'tucuman'),
+  array('id' => '1','nombre' => 'neuquen'),
+  array('id' => '1','nombre' => 'santiago del estero'),
+  array('id' => '2','nombre' => 'artigas'),
+  array('id' => '2','nombre' => 'salto'),
+  array('id' => '2','nombre' => 'rivera'),
+  array('id' => '2','nombre' => 'paysandu'),
+  array('id' => '2','nombre' => 'rio negro'),
+  array('id' => '2','nombre' => 'durazno'),
+  array('id' => '2','nombre' => 'tacuarembo'),
+  array('id' => '2','nombre' => 'cerro largo'),
+  array('id' => '2','nombre' => 'soriano'),
+  array('id' => '2','nombre' => 'flores'),
+  array('id' => '2','nombre' => 'florida'),
+  array('id' => '2','nombre' => 'treinta y tres'),
+  array('id' => '2','nombre' => 'lavalleja'),
+  array('id' => '2','nombre' => 'colonia'),
+  array('id' => '2','nombre' => 'san jose'),
+  array('id' => '2','nombre' => 'rocha'),
+  array('id' => '2','nombre' => 'maldonado'),
+  array('id' => '2','nombre' => 'montevideo')
+);
